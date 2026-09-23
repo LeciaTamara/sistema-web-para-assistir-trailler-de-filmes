f9bf8b16ae1a99a8f9248a1594b2eb7ae45194c4
Lécia Tamara <leciatamara60@gmail.com>
2026-09-23 08:29:03 -0300

    arquivos fimes.html e filmes.css alterado para index.html e index.css

--- site2/javascript.js
+++ site2/javascript.js
@@ -1,9 +1,9 @@
 //tela de login
 function longar() {
-    window.location = "filmes.html"
+    window.location = "index.html"
 }
 
 // botão voltar das páginas dos filmes para a página filme
 function votar() {
-    window.location = "filmes.html"
+    window.location = "index.html"
 }
